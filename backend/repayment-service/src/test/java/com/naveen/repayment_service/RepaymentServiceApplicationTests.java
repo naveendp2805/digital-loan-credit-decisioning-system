747f1f1e5155411f8e1c2c555f21e8984aaca2f8
naveendp2805 <naveendp28122005@gmail.com>
2026-09-06 21:32:19 +0530

    feat(repayment-service): initialize repayment service

---
+++ backend/repayment-service/src/test/java/com/naveen/repayment_service/RepaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naveen.repayment_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepaymentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
